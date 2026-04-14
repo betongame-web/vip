daebfe0b2666194a349f5b4363c65e81937452d6
betongame-web <asimhalder612@gmail.com>
2026-04-14 16:42:38 +0600

    Update LandingLayout.jsx

--- src/components/layouts/LandingLayout.jsx
+++ src/components/layouts/LandingLayout.jsx
@@ -1,3 +1,18 @@
+import NavTopComponent from '@/components/nav/NavTopComponent';
+import BottomNavComponent from '@/components/nav/BottomNavComponent';
+import FooterComponent from '@/components/ui/FooterComponent';
+
 export default function LandingLayout({ children }) {
-  return <main className="min-h-screen px-4 py-8">{children}</main>;
+  return (
+    <>
+      <NavTopComponent />
+
+      <main className="mx-auto min-h-screen max-w-7xl px-4 pb-24 pt-24 md:pb-10">
+        {children}
+        <FooterComponent />
+      </main>
+
+      <BottomNavComponent />
+    </>
+  );
 }
