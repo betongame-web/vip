69c934eb0254736a7035a16ca033ef8a913baa00
betongame-web <asimhalder612@gmail.com>
2026-04-14 16:42:05 +0600

    Update GameLayout.jsx

--- src/components/layouts/GameLayout.jsx
+++ src/components/layouts/GameLayout.jsx
@@ -1,10 +1,16 @@
 import NavTopComponent from '@/components/nav/NavTopComponent';
+import BottomNavComponent from '@/components/nav/BottomNavComponent';
 
 export default function GameLayout({ children }) {
   return (
     <>
       <NavTopComponent />
-      <main className="min-h-screen px-4 pb-8 pt-24">{children}</main>
+
+      <main className="mx-auto min-h-screen max-w-7xl px-4 pb-24 pt-24 md:pb-10">
+        {children}
+      </main>
+
+      <BottomNavComponent />
     </>
   );
 }
